b7c24468a68da23bec337ec3b74307f65ddd25d6
jesse abramson <jr@jesseabramson.com>
2015-10-29 13:43:57 -0400

    bad migration

--- db/migrate/20150724192247_create_campaigns.rb
+++ db/migrate/20150724192247_create_campaigns.rb
@@ -8,8 +8,10 @@ class CreateCampaigns < ActiveRecord::Migration
       t.string :hub
       t.string :token
       t.string :video
+      t.string :custom_path, null: false
       t.text :details
       
       t.timestamps null: false
     end
   end
+end
--- db/migrate/20151029153307_add_custom_path_to_campaigns.rb
+++
@@ -1,5 +0,0 @@
-class AddCustomPathToCampaigns < ActiveRecord::Migration
-  def change
-    add_column :campaigns, :custom_path, :string, null: false
-  end
-end
