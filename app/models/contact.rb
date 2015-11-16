0e90907e3ddf40b4de97ed7c01dc79adeafcfeee
jesse abramson <jr@jesseabramson.com>
2015-11-16 15:44:51 -0500

    style fix

--- app/models/contact.rb
+++ app/models/contact.rb
@@ -1,9 +1,6 @@
 class Contact < ActiveRecord::Base
-  # Include default devise modules. Others available are:
-  # :confirmable, :lockable, :timeoutable and :omniauthable
   devise :database_authenticatable, :registerable,
          :recoverable, :rememberable, :trackable, :validatable
-         # , :confirmable
 
   has_many :campaigns
   has_many :referrals, through: :campaign
